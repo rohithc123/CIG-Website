1fcc481f58ce7ecc1f6d4ed08b74cd9b1a6626fa
Rohithc123 <rohithc.eec.cseb@gmail.com>
2024-12-23 21:56:05 +0530

    feat:File upload using multer (Note: This is just a demo part not the final file upload code)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,29 @@
+//TODO set up rate limit
+//TODO allow API access only from certain domains
+
 import express from "express";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../backend/uploads");
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname + "-" + Date.now());
+  },
+});
+
+const upload = multer({ storage: storage });
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.send("File uploaded successfully");
 });
 
 app.listen(port, () => {
