c48493b2017c26dde7e66228ec7675b2ab96f0ad
Rohithc123 <rohithc.eec.cseb@gmail.com>
2024-12-25 19:37:38 +0530

    feat:Adding name , designation fields and also validator for name,designation,email. match regression changes of linkedin

--- backend/model/team.ts
+++ backend/model/team.ts
@@ -1,20 +1,51 @@
 import mongoose, { Schema, model, Document } from "mongoose";
 
-interface IAbout extends Document {
+interface ITeam extends Document {
+  name: string;
+  designation: string;
   email: string;
   phoneNo: number;
   linkedin: string;
 }
 
-const aboutSchema = new Schema<IAbout>({
+const teamSchema = new Schema<ITeam>({
+  name: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v: string) {
+        return /^[A-Za-z\s]+$/.test(v);
+      },
+      message: "Name should contain only letters and spaces",
+    },
+  },
+  designation: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v: string) {
+        return /^[A-Za-z\s]+$/.test(v);
+      },
+      message: "Designation should contain only letters and spaces",
+    },
+  },
   email: { type: String, required: true, match: /.+\@.+\..+/ },
-  phoneNo: { type: Number, required: true, match: /^[6-9]\d{9}$/ },
+  phoneNo: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (v: number) {
+        return /\d{10}/.test(v.toString());
+      },
+      message: "Phone number should have exactly 10 digits",
+    },
+  },
   linkedin: {
     type: String,
     required: true,
-    match: /^https:\/\/[a-z]{2,3}\.linkedin\.com\/.*$/,
+    match: /^https:\/\/(www\.)?linkedin\.com\/in\/.*$/,
   },
 });
 
-const About = mongoose.models.About || model<IABout>("About", aboutSchema);
-export default About;
+const Team = mongoose.models.Team || model<ITeam>("Team", teamSchema);
+export default Team;
