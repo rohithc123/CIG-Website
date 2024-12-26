691cdc9614f96a99c7f98d5d4a0a11f8251b3226
Sunil <sunilmachra2021@gmail.com>
2024-12-26 14:47:03 +0530

    fix: update file naming convention in multer storage

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -35,7 +35,7 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.originalname + "-" + Date.now());
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
